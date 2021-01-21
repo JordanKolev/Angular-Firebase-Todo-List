8798b7be26d14633bcc35b929c2caf47ccc52bfc
Jordan Kolev <jordan.g.kolev@gmail.com>
2021-01-21 18:19:13 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+export const environment = { 
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyCYRP1mii-s3gW995aio5_YYh0AGu3lxAc",
+    authDomain: "todo-app-4d952.firebaseapp.com",
+    databaseURL: "https://todo-app-4d952-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todo-app-4d952",
+    storageBucket: "todo-app-4d952.appspot.com",
+    messagingSenderId: "863041405005",
+    appId: "1:863041405005:web:f406e7240a45ddd637dc04" 
+  }
+  
+};  
 
 /*
  * For easier debugging in development mode, you can import the following file
